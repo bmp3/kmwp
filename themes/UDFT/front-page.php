3460f78aec6112957386f91ede9c75bf51bce110
you@example.com <you@example.com>
2018-05-25 10:01:41 +0300

    may 25, main page

--- themes/UDFT/front-page.php
+++ themes/UDFT/front-page.php
@@ -22,21 +22,21 @@ echo '
 						<div class="s-img-box"><a href="#"><img src="' . get_stylesheet_directory_uri() . '/img/img1_1.png"></a></div>
 						<div class="s-content-box">
 							<h3 class="s-title">Growth Hormone Deficiency Treatment</h3>
-							<div class="s-content">The best hormone replacement therapy programs combine medical innovation with human compassion, and that is what Nexel Medical</div>
+							<div class="s-content">Increase your energy! Take the first step to a stronger, slimmer, more vital you, with growth hormone replacement therapy.</div>
 						</div>
 					</div>
 					<div class="single-service col col-lg-4 col-12">
 						<div class="s-img-box"><a href="#"><img src="' . get_stylesheet_directory_uri() . '/img/img2_1.png"></a></div>
 						<div class="s-content-box">
 							<h3 class="s-title">Low Testosterone Treatment</h3><br>
-							<div class="s-content">The best hormone replacement therapy programs combine medical innovation with human compassion, and that is what Nexel Medical</div>
+							<div class="s-content">Stop missing out on doing the things you love in life because of low testosterone. Get started today, and see if you qualify for testosterone therapy.</div>
 						</div>
 					</div>
 					<div class="single-service col col-lg-4 col-12">
 						<div class="s-img-box"><a href="#"><img src="' . get_stylesheet_directory_uri() . '/img/img3_2.png"></a></div>
 						<div class="s-content-box">
 							<h3 class="s-title">Blood Test for Growth Hormone & Testosterone Levels</h3>
-							<div class="s-content">The best hormone replacement therapy programs combine medical innovation with human compassion, and that is what Nexel Medical</div>
+							<div class="s-content">It all starts with having your hormone levels checked and evaluated by our team of experts. Call today to schedule your appointment!</div>
 						</div>
 					</div>
 				</div>
@@ -49,8 +49,10 @@ echo '
 		<article>
 			<div class="container">
 				<div class="p1 parallax-box">
-					<h2 class="section-title">You are in Good Company</h2>
-					<div class="content-box">Kingsberg Medical HGH Testosterone Institute specializes in the most effective treatments for HGH & Testosterone Replacement therapies.</div>
+					<h2 class="section-title">Experience You Can Trust - Compassion You Can Count On</h2>
+					<div class="content-box">Kingsberg Medical specializes in the safest, most effective, medically proven, Growth Hormone & Testosterone Replacement therapies.<br><br>
+Over the past 10 years we have helped more than 14,000 men and women just like you, to increase energy, improve focus, and reignite passion, so that they can be their best at any age!
+</div>
 				</div>
 			</div>
 		</article>
@@ -68,7 +70,7 @@ echo '
 						<div class="exct-box tab2-trigger col col-lg-6 col-md-12 col-12">
 							<div class="exct">
 								<h3 class="exct-title">Benefits of Growth Hormone Therapy</h3>
-								<div class="exct-content">The best hormone replacement therapy programs combine medical innovation with human compassion, and that is what Nexel Medical delivers.</div>
+								<div class="exct-content">For men or women who are suffering from adult onset, or age-related growth hormone deficiency, growth hormone therapy can return quality of life to you. Growth hormone therapy has a remarkable number of positive benefits. Growth hormone therapy will help you lose weight, build more lean muscle, strengthen your bones, increase you energy, and improve heart health.</div>
 							</div>
 						</div>
 						<div class="impr-content-box tab2-data col col-lg-6 col-md-12 col-12">
@@ -76,16 +78,13 @@ echo '
 								<div class="impr-img"><img src="' . get_stylesheet_directory_uri() . '/img/Benefits-of-HGH-Therapy.jpg"></div>
 								<div class="impr-list-box">
 									<ul class="impr-list">
-										<li class="impr-item"><span>Muscle gain</span></li>
-										<li class="impr-item"><span>Weight loss</span></li>
-										<li class="impr-item"><span>Better sleep</span></li>
-										<li class="impr-item"><span>Sexual desire and help with ED (erectile disfunction)</span></li>
-										<li class="impr-item"><span>Great amounts of energy, vigor and vitality</span></li>
-										<li class="impr-item"><span>Immune system</span></li>
-										<li class="impr-item"><span>Stronger bone mineral density</span></li>
-										<li class="impr-item"><span>Lower cholesterol levels and better heart health</span></li>
-										<li class="impr-item"><span>Younger looking appearance</span></li>
-										<li class="impr-item"><span>Improved mental and emotional health</span></li>
+										<li class="impr-item"><span>Increased energy and vitality - HGH Therapy boosts your metabolism, resulting in increased energy</span></li>
+										<li class="impr-item"><span>More lean muscle - HGH helps improves protein synthesis, which helps you build more lean muscle</span></li>
+										<li class="impr-item"><span>Lose weight, particularly belly fat - HGH therapy boosts your metabolism, which helps you burn fat better</span></li>
+										<li class="impr-item"><span>Better sleep - HGH is a hormone that is essential to regulating sleep</span></li>
+										<li class="impr-item"><span>Stronger bones - HGH is essential to bone health and improves bone density</span></li>
+										<li class="impr-item"><span>Improved immune system - HGH rejuvenates every cell in your body. It reduces inflammation, and therefore boosts your immune system</span></li>
+										<li class="impr-item"><span>Younger looking skin and overall appearance - HGH rejuvenates skin cells and improves collagen production, resulting in smoother, younger looking skin</span></li>
 									</ul>
 								</div>
 							</div>
@@ -96,7 +95,7 @@ echo '
 						<div class="exct-box tab2-trigger col col-lg-6 col-md-12 col-12">
 							<div class="exct">
 								<h3 class="exct-title">Benefits of Testosterone Therapy</h3>
-								<div class="exct-content">The best hormone replacement therapy programs combine medical innovation with human compassion, and that is what Nexel Medical delivers.</div>
+								<div class="exct-content">Low testosterone can effect both men and women. For anyone with low testosterone, but particularly for men over 40, testosterone therapy can improve strength and vitality. It can increase your sex drive and improve sexual performance. Other benefits of testosterone therapy include: increased energy, weight loss, improved muscle mass, improved cognition and emotional well-being.</div>
 							</div>
 						</div>
 						<div class="impr-content-box tab2-data col col-lg-6 col-md-12 col-12">
@@ -104,16 +103,13 @@ echo '
 								<div class="impr-img"><img src="' . get_stylesheet_directory_uri() . '/img/Benefits-of-Testosterone-Therapy.jpg"></div>
 								<div class="impr-list-box">
 									<ul class="impr-list">
-										<li class="impr-item"><span>Muscle gain</span></li>
-										<li class="impr-item"><span>Weight loss</span></li>
-										<li class="impr-item"><span>Better sleep</span></li>
-										<li class="impr-item"><span>Sexual desire and help with ED (erectile disfunction)</span></li>
-										<li class="impr-item"><span>Great amounts of energy, vigor and vitality</span></li>
-										<li class="impr-item"><span>Immune system</span></li>
-										<li class="impr-item"><span>Stronger bone mineral density</span></li>
-										<li class="impr-item"><span>Lower cholesterol levels and better heart health</span></li>
-										<li class="impr-item"><span>Younger looking appearance</span></li>
-										<li class="impr-item"><span>Improved mental and emotional health</span></li>
+										<li class="impr-item"><span>Increased energy and vitality - Testosterone boosts metabolism, improves blood flow, and improves liver function, all of which give you more energy and stamina</span></li>
+										<li class="impr-item"><span>More lean muscle - Testosterone improves protein synthesis and helps you build muscle</span></li>
+										<li class="impr-item"><span>Lose weight, particularly belly fat - Testosterone fuels lipolysis, or the burning of fat</span></li>
+										<li class="impr-item"><span>Improved sex drive and help with erectile dysfunction - Testosterone boosts libido or sex drive, and helps men with ED by improving blood flow and increasing nitric oxide production</span></li>
+										<li class="impr-item"><span>Improved heart health - Testosterone improves blood flow and can lower your risk of cardiovascular disease</span></li>
+										<li class="impr-item"><span>Improved concentration and memory - Testosterone is a precursor to certain neurotransmitters that are closely linked to memory and cognition</span></li>
+										<li class="impr-item"><span>Improved emotional health - Testosterone is a precursor to certain neurotransmitters that are closely linked to feeling more positive and less anxious or depressed</span></li>
 									</ul>
 								</div>
 							</div>
@@ -124,7 +120,7 @@ echo '
 						<div class="exct-box tab2-trigger col col-lg-6 col-md-12 col-12">
 							<div class="exct">
 								<h3 class="exct-title">Benefits of Sermorelin Therapy</h3>
-								<div class="exct-content">The best hormone replacement therapy programs combine medical innovation with human compassion, and that is what Nexel Medical delivers.</div>
+								<div class="exct-content">Sermorelin Therapy is a lower-cost alternative to growth hormone therapy. For some patients, Sermorelin therapy can deliver similar results to growth hormone therapy, such as: increased metabolism, increased muscle mass, weight loss, and improved vitality. Sermorelin therapy is often used in conjunction with growth hormone therapy or testosterone therapy.</div>
 							</div>
 						</div>
 						<div class="impr-content-box tab2-data col col-lg-6 col-md-12 col-12">
@@ -132,16 +128,12 @@ echo '
 								<div class="impr-img"><img src="' . get_stylesheet_directory_uri() . '/img/Benefits-of-Sermorelin-Therapy.jpg"></div>
 								<div class="impr-list-box">
 									<ul class="impr-list">
-										<li class="impr-item"><span>Muscle gain</span></li>
-										<li class="impr-item"><span>Weight loss</span></li>
-										<li class="impr-item"><span>Better sleep</span></li>
-										<li class="impr-item"><span>Sexual desire and help with ED (erectile disfunction)</span></li>
-										<li class="impr-item"><span>Great amounts of energy, vigor and vitality</span></li>
-										<li class="impr-item"><span>Immune system</span></li>
-										<li class="impr-item"><span>Stronger bone mineral density</span></li>
-										<li class="impr-item"><span>Lower cholesterol levels and better heart health</span></li>
-										<li class="impr-item"><span>Younger looking appearance</span></li>
-										<li class="impr-item"><span>Improved mental and emotional health</span></li>
+										<li class="impr-item"><span>Increased energy and vitality - By increasing the production of HGH, sermorelin therapy boosts your metabolism, resulting in increased energy</span></li>
+										<li class="impr-item"><span>More lean muscle - Sermorelin aids in protein synthesis, which helps you build more lean muscle</span></li>
+										<li class="impr-item"><span>Lose weight, particularly belly fat - Sermorelin therapy can boost your metabolism, which helps you burn fat better</span></li>
+										<li class="impr-item"><span>Stronger bones - By increasing the production of HGH, sermorelin therapy improves bone density</span></li>
+										<li class="impr-item"><span>Improved immune system - By increasing the production of HGH, sermorelin therapy ramps up your immune system and decreases systemic inflammation</span></li>
+										<li class="impr-item"><span>Younger looking skin and overall appearance - Sermorelin therapy rejuvenates skin cells and improves collagen production, resulting in smoother, younger looking skin</span></li>
 									</ul>
 								</div>
 							</div>
@@ -170,7 +162,7 @@ echo '
 						<div class="pref-content-box">
 							<h3>Contact Kingsberg Medical</h3>
 							<div class="pref-content">
-								The best hormone replacement therapy programs combine medical innovation with human comprassion, and that is what Nexel Medical delivers
+								Fill out &quot;get started&quot; form or Call us!<br>That&lsquo;s it... it&lsquo;s easy
 							</div>
 						</div>
 					</div>
@@ -184,7 +176,7 @@ echo '
 						<div class="pref-content-box">
 							<h3>Blood Work and Diagnostic</h3>
 							<div class="pref-content">
-								The best hormone replacement therapy programs combine medical innovation with human comprassion, and that is what Nexel Medical delivers
+								With a zip code, we will schedule for blood testing and a physical exam near your home or office.
 							</div>
 						</div>
 					</div>
@@ -198,7 +190,7 @@ echo '
 						<div class="pref-content-box">
 							<h3>Get Prescription From a Doctor</h3>
 							<div class="pref-content">
-								The best hormone replacement therapy programs combine medical innovation with human comprassion, and that is what Nexel Medical delivers
+								If your lab work indicates GH deficiency or low Testosterone, one of our local doctors will prescribe you all necessary hormone replacement therapies.
 							</div>
 						</div>
 					</div>
